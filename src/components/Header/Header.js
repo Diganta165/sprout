5a577189763be5aa3292546959ed3a4af4426fa9
Diganta165 <mdsrdiganta@gmail.com>
2023-01-16 23:50:39 +0600

    header initial state started

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import "./Header.css"
 const Header = () => {
     return (
         <>
-            <div>
-                This is Header
+            <div className='header_section'>
+                <div className='header_business_logo_wrapper'>
+                    <img src='https://www.clipartmax.com/png/middle/151-1517829_designs-for-business-logos-portland-logo-design-services-design.png' />
+                </div>
+                <div className='breadcrumb'>
+                    <span className='line_1'></span>
+                    <span className='line_2'></span>
+                    <span className='line_3'></span>
+                </div>
             </div>  
         </>
     );
